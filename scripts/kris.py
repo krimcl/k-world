247e5fccf578e857f0f04c6bd1405022e5b6fd63
root <root@linux.suse>
2017-03-20 17:46:51 -0700

    commit 2

--- scripts/kris.py
+++ scripts/kris.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 #dicetest
 #by kris
-import dice
+import dice, sys
 
+numberofdice = int(sys.argv[1]) #pulls argument after script name for number of 
+dice to roll
 
-rollit = dice.roll_d4(3)
+rollit = dice.roll_d4(numberofdice)
 print rollit
